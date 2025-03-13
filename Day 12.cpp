6c35bdcd1a2cbeb3ff177b3aad36e3cc5d400eb8
Pranjal Shukla <prjacer@gmail.com>
2025-03-13 19:33:40 +0530

    Update Day 12.cpp

--- Day 12.cpp
+++ Day 12.cpp
@@ -29,8 +29,50 @@ public:
 
 // Q2 : Find first and last position of element in sorted array
 
-// Approach : 
+// Approach : To use to lower bound on target +1 to get upper limit and and upper bound on target -1 to get lower limit 
 
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2,-1) ;
+
+        if(binarySearch(nums,0,nums.size()-1 , target)==-1){
+            return ans; 
+        }
+
+        else {
+            vector<int>::iterator lower;
+            vector<int>::iterator upper;
+            lower=upper_bound(nums.begin(),nums.end(),target-1);
+            upper=lower_bound(nums.begin(),nums.end(),target+1);
+            ans[0]=lower-nums.begin();
+            ans[1]=upper-nums.begin()-1;
+        }
+        
+        return ans;
+        
+    }
+private :
+int binarySearch(vector<int>& arr , int low , int high , int target ){
+    int mid ;
+    while(low<=high){
+        mid= low + (high-low)/2 ;
+        if(arr[mid]==target){
+            return mid ;
+        }
+        else if (arr[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1 ;
+}
+};
+
+//Time Complexity : O(logn)
+//Space Complexity : O(1) 
 
 
 // Q3 : Search Insert Position 
